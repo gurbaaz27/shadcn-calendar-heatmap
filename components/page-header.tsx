c4b82c9486c39886c8df3be04a2e73fe23a16a5e
Gurbaaz Singh Nandra <gurbaazsn123@gmail.com>
2024-06-24 22:49:18 +0530

    Padding on top reduced

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -8,7 +8,7 @@ function PageHeader({
   return (
     <section
       className={cn(
-        "mx-auto flex max-w-[800px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20",
+        "mx-auto flex max-w-[800px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-18 lg:pb-16",
         className
       )}
       {...props}
